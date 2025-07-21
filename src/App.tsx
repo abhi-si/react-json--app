e7460c4ae2fc1391aee2eb83870b3d4edaa40007
Abhishek Singh <abhishek@lawyerr.>
2025-07-21 12:03:11 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
 import { SchemaBuilder }  from './components/SchemaBuilder';
-import { Toaster } from './components/ui/sonner';
+
 
 function App() {
   return (
     <>
       <SchemaBuilder />
-      <Toaster />
+      
     </>
   );
 }
